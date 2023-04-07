9446690fe66a8f2ddaccbc290ae9236be211e07b
Victor Flávio Demarchi Viana <victor.fl.de.vi@gmail.com>
2023-04-07 17:44:41 -0300

    questao 2 respondida em script.js e resultado.docx

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ let num1;
 num1 = undefined;
 
 console.log(num1);
+
+//numero 2 
+//Como usar o operador % em JavaScript? Exemplifique e demonstre a saída!
+
+let resultado = 10 % 3;
+console.log(resultado);
+
+let numero = 7;
+if (numero % 2 === 0) {
+  console.log("O número é par");
+} else {
+  console.log("O número é ímpar");
+}
