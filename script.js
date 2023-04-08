e5da92f50a49377be2f1a61b97db744c8bae14a3
Vinícius <Vini.bernardino2004@gmail.com>
2023-04-08 16:52:35 -0300

    Perguntas 4 e 5 + ajuste word

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 
 //numero 1 
 //declarar variavel com valor undefined
-let num1;
+/*let num1;
 //atribuir valor a variavel
 num1 = undefined;
 
 document.write(num1);
+document.write("<br><br>")
+
+
 
 //numero 2 
 //Como usar o operador % em JavaScript? Exemplifique e demonstre a saída!
 
 let resultado = 10 % 3;
 document.write(resultado);
+document.write("<br><br>")
 
 let numero = 7;
-if (numero % 2 === 0) {
+if (numero % 2 == 0) {
   document.write("O número é par");
 } else {
   document.write("O número é ímpar");
 }
+document.write("<br><br>")
 
 //numero 3
 //  - Escreva um programa que calcula o índice de massa corporal (IMC) de uma pessoa e exibe uma 
@@ -40,7 +45,54 @@ if (imc < 18.5) {
 } else {
   document.write("Seu IMC é " + imc.toFixed(2) + ", você está acima do peso.");
 }
+document.write("<br><br>")
 
+
+// numero 4
+/*Faça um programa que receba a idade de uma pessoa e exiba uma mensagem indicando em qual faixa
+etária ela se encontra: criança (até 12 anos), adolescente (entre 13 e 18 anos), adulto (entre 19 e 60 anos)
+ou idoso (mais de 60 anos). Exemplifique e demonstre a saída!*/
+/*
+// declarar variável
+let idade = 18;
+
+if (idade <= 12){
+  document.write("É uma criança.")
+}else if (13 <= idade <= 18){
+  document.write("É um adolescente.")
+}else if (19 <= idade <= 60){
+  document.write("É um adulto.")
+}else {
+  document.write("É um idoso.")
+}
+document.write("<br><br>")
+
+*/
+// numero 5
+/*Crie um programa que receba um nome de usuário e uma senha e verifique se eles correspondem aos
+dados de um usuário cadastrado. Considere que o usuário cadastrado possui nome de usuário "admin" e
+senha "1234". O programa deve exibir uma mensagem indicando se o login foi realizado com sucesso ou
+se houve falha de autenticação. Exemplifique e demonstre a saída!*/
+
+// declaração de variaveis
+let userName = prompt("Digite o nome de usuário", "Nome de Usuário");
+let senha = prompt("Digite a senha", "Senha");
+
+if (userName == "admin" && senha == "1234"){
+  document.write("Dados verificados.");
+}else {
+  document.write("Nome de usuário ou senha incorretos.")
+}
+document.write("<br><br>")
+
+
+
+
+
+
+
+
+/*
 //numero 8
 
 //Crie um programa em Javascript que troque a palavra lugar por mundo da frase O LUGAR VIRA 
@@ -50,6 +102,7 @@ const frase = "O LUGAR VIRA TECNOLOGIA";
 const novaFrase = frase.replace("LUGAR", "MUNDO");
 
 document.getElementById("texto").innerHTML = novaFrase;
+document.write("<br><br>")
 
 //numero 9
 // Crie um programa que declare a variável como String depois converta para float e em seguida apresente 
@@ -59,3 +112,4 @@ let valorString = "444.41441";
 let valorFloat = parseFloat(valorString).toFixed(2);
 
 document.write(valorFloat) // irá exibir 444.41
+*/
